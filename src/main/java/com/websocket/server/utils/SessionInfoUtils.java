efdd0fdb7692360ea5bfa6c2bc784780ac61e37f
GabryelMelo07 <gabryelcraft@gmail.com>
2025-02-25 18:03:14 -0300

    Adjusting null pointer exception

--- src/main/java/com/websocket/server/utils/SessionInfoUtils.java
+++ src/main/java/com/websocket/server/utils/SessionInfoUtils.java
@@ -3,17 +3,19 @@ package com.websocket.server.utils;
 import org.springframework.web.socket.WebSocketSession;
 
 public class SessionInfoUtils {
-    
+
     public static void printSessionInfo(WebSocketSession session) {
+        String principalName = (session.getPrincipal() != null) ? session.getPrincipal().getName() : null;
+
         SessionInfo sessionInfo = new SessionInfo(
             session.getId(),
-            session.getUri().toString(),
-            session.getPrincipal().getName(),
-            session.getLocalAddress().toString(),
-            session.getRemoteAddress().toString()
+            (session.getUri() != null) ? session.getUri().toString() : null,
+            principalName,
+            (session.getLocalAddress() != null) ? session.getLocalAddress().toString() : null,
+            (session.getRemoteAddress() != null) ? session.getRemoteAddress().toString() : null
         );
-        
+
         System.out.println(sessionInfo);
     }
-    
+
 }
